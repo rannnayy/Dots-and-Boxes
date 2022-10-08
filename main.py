d762f2c4ce89afd3632ca8eef2af08a39ec29e6e
Maharani Ayu Putri Irawan <72875641+rannnayy@users.noreply.github.com>
2022-10-08 16:54:49 +0700

    Finished LocalSearchBot, still error on move making

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from GameState import GameState
 
 # * Import your bot
 from RandomBot import RandomBot
+from LocalSearchBot import LocalSearchBot
 
 size_of_board = 600
 number_of_dots = 4
@@ -314,5 +315,5 @@ if __name__ == "__main__":
     PvB mode: game_instance = Dots_and_Boxes(None, BotName()) or game_instance = Dots_and_Boxes(BotName(), None)
     BvB mode: game_instance = Dots_and_Boxes(BotName(), BotName())
     """
-    game_instance = Dots_and_Boxes(None, RandomBot())
+    game_instance = Dots_and_Boxes(LocalSearchBot(), RandomBot())
     game_instance.mainloop()
