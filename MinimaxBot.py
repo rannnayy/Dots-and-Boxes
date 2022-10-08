1291cbdec47d0caba994eded11dabcceff4cd345
Zeus-s <kristabdixx@gmail.com>
2022-10-08 18:17:32 +0700

    fix bug type error, still error oob

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -11,16 +11,16 @@ class MinimaxBot(Bot):
         pass
 
     def get_action(self, state: GameState) -> GameAction:
-        _,bestAction = self.minimax(state,True,-9999,9999)
+        _, bestAction = self.minimax(state, True, -9999, 9999)
         return bestAction
 
     def utility_value(self, state : GameState) -> int:
         if state.player1_turn:
-            return np.count_nonzero(state.board_status==-4)
+            return len(np.argwhere(state.board_status == -4))
         else:
-            return np.count_nonzero(state.board_status==4)
+            return len(np.argwhere(state.board_status == 4))
 
-    def get_next_turn(self,state : GameState, act : GameAction) -> tuple[bool,GameState]:
+    def get_next_turn(self, state : GameState, act : GameAction) -> (bool, GameState):
         state_copy = deepcopy(state)
         x = act.position[0]
         y = act.position[1]
@@ -29,37 +29,38 @@ class MinimaxBot(Bot):
         scored = False
         if state.player1_turn:
             playerModifier = -1
-        if act.action_type=='row':
+
+        if act.action_type == 'row':
             state_copy.row_status[x][y] = playerModifier
         else :
             state_copy.col_status[x][y] = playerModifier
             
 
-        if y < (MinimaxBot.num_of_square-1) and x < (MinimaxBot.num_of_square-1):
+        if y < (MinimaxBot.num_of_square - 1) and x < (MinimaxBot.num_of_square - 1):
             state_copy.board_status[y][x] = (abs(state_copy.board_status[y][x]) + val) * playerModifier
             if abs(state_copy.board_status[y][x]) == 4:
                 scored = True
 
-        if type == 'row':
+        if act.action_type == 'row':
             state_copy.row_status[y][x] = 1
             if y >= 1:
                 state_copy.board_status[y-1][x] = (abs(state_copy.board_status[y-1][x]) + val) * playerModifier
                 if abs(state_copy.board_status[y-1][x]) == 4:
                     scored = True
-
-        elif type == 'col':
+        elif act.action_type == 'col':
             state_copy.col_status[y][x] = 1
             if x >= 1:
                 state_copy.board_status[y][x-1] = (abs(state_copy.board_status[y][x-1]) + val) * playerModifier
                 if abs(state_copy.board_status[y][x-1]) == 4:
                     scored = True
+
         print(scored,state_copy.player1_turn)
         if scored :
-            return state_copy.player1_turn,state_copy
+            return state_copy.player1_turn, state_copy
         else:
             return not state_copy.player1_turn, state_copy
 
-    def minimax(self,state : GameState, isMaximize : bool, alpha : int, beta : int) -> tuple[int,GameAction]:
+    def minimax(self, state : GameState, isMaximize : bool, alpha : int, beta : int) -> (int,GameAction):
         bot_code = state.player1_turn
         print(state.row_status)
         if ((state.row_status == 1).all() and (state.col_status == 1).all()):
@@ -68,13 +69,12 @@ class MinimaxBot(Bot):
         MAX, MIN = 1000, -1000
         row_choices = [(x,y) for x in range(MinimaxBot.num_of_square) for y in range(MinimaxBot.num_of_square-1)]
         col_choices = [(x,y) for x in range(MinimaxBot.num_of_square-1) for y in range(MinimaxBot.num_of_square)]
-
         if isMaximize:
             bestValue = -9999
             bestStep = None
             for i,j in row_choices :
                 if state.row_status[i][j] == 0 :
-                    next_turn,state_copy = self.get_next_turn(state,GameAction('row',(i,j)))
+                    next_turn, state_copy = self.get_next_turn(state, GameAction('row', (i,j)))
                     print(state_copy.row_status)
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue < value :
@@ -85,7 +85,7 @@ class MinimaxBot(Bot):
                         break
             for i,j in col_choices :
                 if state.col_status[i][j] == 0 :
-                    next_turn,state_copy = self.get_next_turn(state,GameAction('col',(i,j)))
+                    next_turn, state_copy = self.get_next_turn(state, GameAction('col', (i,j)))
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue < value :
                         bestValue = value
@@ -101,7 +101,7 @@ class MinimaxBot(Bot):
             bestStep = None
             for i,j in row_choices:
                 if state.row_status[i][j] == 0 :
-                    next_turn,state_copy = self.get_next_turn(state,GameAction('row',(i,j)))
+                    next_turn, state_copy = self.get_next_turn(state, GameAction('row', (i,j)))
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue > value :
                         bestValue = value
@@ -111,7 +111,7 @@ class MinimaxBot(Bot):
                         break
             for i,j in col_choices:
                 if state.col_status[i][j] == 0 :
-                    next_turn,state_copy = self.get_next_turn(state,GameAction('col',(i,j)))
+                    next_turn, state_copy = self.get_next_turn(state, GameAction('col', (i,j)))
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue > value :
                         bestValue = value
--- main.py
+++ main.py
@@ -11,11 +11,8 @@ from GameState import GameState
 
 # * Import your bot
 from RandomBot import RandomBot
-<<<<<<< HEAD
 from MinimaxBot import MinimaxBot
-=======
 from LocalSearchBot import LocalSearchBot
->>>>>>> d762f2c4ce89afd3632ca8eef2af08a39ec29e6e
 
 size_of_board = 600
 number_of_dots = 4
@@ -319,5 +316,5 @@ if __name__ == "__main__":
     PvB mode: game_instance = Dots_and_Boxes(None, BotName()) or game_instance = Dots_and_Boxes(BotName(), None)
     BvB mode: game_instance = Dots_and_Boxes(BotName(), BotName())
     """
-    game_instance = Dots_and_Boxes(LocalSearchBot(), RandomBot())
+    game_instance = Dots_and_Boxes(None, MinimaxBot())
     game_instance.mainloop()
