c843351a7a0077c3b31484403bc289ea56753500
rizkyramadhana26 <rizkyramadhana26@gmail.com>
2022-10-15 07:13:36 +0700

    perbaiki giliran

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -4,6 +4,7 @@ from GameState import GameState
 import random
 import numpy as np
 from copy import deepcopy
+from time import sleep
 
 class MinimaxBot(Bot):
     num_of_square = 4
@@ -11,6 +12,10 @@ class MinimaxBot(Bot):
         pass
 
     def get_action(self, state: GameState) -> GameAction:
+        print("AWAL")
+        print("player1_turn",state.player1_turn)
+        print("col_status:",state.col_status)
+        print("row_status:",state.row_status)
         _, bestAction = self.minimax(state, True, -9999, 9999)
         return bestAction
 
@@ -20,10 +25,10 @@ class MinimaxBot(Bot):
         else:
             return len(np.argwhere(state.board_status == 4))
 
-    def get_next_turn(self, state : GameState, act : GameAction) -> (bool, GameState):
+    def get_next_turn(self, state : GameState, act : GameAction) -> tuple[bool, GameState]:
         state_copy = deepcopy(state)
-        x = act.position[0]
-        y = act.position[1]
+        y = act.position[0]
+        x = act.position[1]
         val = 1
         playerModifier = 1
         scored = False
@@ -31,9 +36,9 @@ class MinimaxBot(Bot):
             playerModifier = -1
 
         if act.action_type == 'row':
-            state_copy.row_status[x][y] = playerModifier
+            state_copy.row_status[y][x] = playerModifier
         else :
-            state_copy.col_status[x][y] = playerModifier
+            state_copy.col_status[y][x] = playerModifier
             
 
         if y < (MinimaxBot.num_of_square - 1) and x < (MinimaxBot.num_of_square - 1):
@@ -53,16 +58,15 @@ class MinimaxBot(Bot):
                 state_copy.board_status[y][x-1] = (abs(state_copy.board_status[y][x-1]) + val) * playerModifier
                 if abs(state_copy.board_status[y][x-1]) == 4:
                     scored = True
-
-        print(scored,state_copy.player1_turn)
         if scored :
             return state_copy.player1_turn, state_copy
         else:
-            return not state_copy.player1_turn, state_copy
+            #state_copy.player1_turn = not state_copy.player1_turn
+            return not state_copy.player1_turn, GameState(board_status=state_copy.board_status,row_status=state_copy.row_status,col_status=state_copy.col_status,player1_turn=not state_copy.player1_turn)#state_copy
 
-    def minimax(self, state : GameState, isMaximize : bool, alpha : int, beta : int) -> (int,GameAction):
-        bot_code = state.player1_turn
-        print(state.row_status)
+    def minimax(self, state : GameState, isMaximize : bool, alpha : int, beta : int) -> tuple[int,GameAction]:
+        bot_code = False #karena player2#state.player1_turn
+        print("bot_code: ",bot_code)
         if ((state.row_status == 1).all() and (state.col_status == 1).all()):
             return (self.utility_value(state),None)
 
@@ -75,24 +79,37 @@ class MinimaxBot(Bot):
             for i,j in row_choices :
                 if state.row_status[i][j] == 0 :
                     next_turn, state_copy = self.get_next_turn(state, GameAction('row', (i,j)))
-                    print(state_copy.row_status)
+                    print("masuk max1")
+                    print("player1_turn: ",state.player1_turn)
+                    print("row_status:\n",state_copy.row_status)
+                    print("col_status:\n",state_copy.col_status)
+                    #state_copy.player1_turn=next_turn
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue < value :
                         bestValue = value
-                        bestStep = GameAction('row',(j,i))
+                        bestStep = GameAction('row',(i,j))
                     alpha = max(alpha,bestValue)
+                    print("alpha: ",alpha,"beta: ",beta)
                     if beta <= alpha :
                         break
+                    #sleep(1)
             for i,j in col_choices :
                 if state.col_status[i][j] == 0 :
                     next_turn, state_copy = self.get_next_turn(state, GameAction('col', (i,j)))
+                    print("masuk max2")
+                    print("player1_turn: ",state.player1_turn)
+                    print("row_status:\n",state_copy.row_status)
+                    print("col_status:\n",state_copy.col_status)
+                    #state_copy.player1_turn=next_turn
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue < value :
                         bestValue = value
                         bestStep = GameAction('col',(j,i))
                     alpha = max(alpha,bestValue)
+                    print("alpha: ",alpha,"beta: ",beta)
                     if beta <= alpha :
                         break
+                    #sleep(1)
 
             return bestValue, bestStep
 
@@ -102,6 +119,11 @@ class MinimaxBot(Bot):
             for i,j in row_choices:
                 if state.row_status[i][j] == 0 :
                     next_turn, state_copy = self.get_next_turn(state, GameAction('row', (i,j)))
+                    print("masuk min1")
+                    print("player1_turn: ",state.player1_turn)
+                    print("row_status:\n",state_copy.row_status)
+                    print("col_status:\n",state_copy.col_status)
+                    #state_copy.player1_turn=next_turn
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue > value :
                         bestValue = value
@@ -112,6 +134,11 @@ class MinimaxBot(Bot):
             for i,j in col_choices:
                 if state.col_status[i][j] == 0 :
                     next_turn, state_copy = self.get_next_turn(state, GameAction('col', (i,j)))
+                    print("masuk min2")
+                    print("player1_turn: ",state.player1_turn)
+                    print("row_status:\n",state_copy.row_status)
+                    print("col_status:\n",state_copy.col_status)
+                    #state_copy.player1_turn=next_turn
                     value,_ = self.minimax(state_copy,next_turn==bot_code,alpha,beta)
                     if bestValue > value :
                         bestValue = value
