ddbb9817ebe31470f5a911a15134e6f91d890394
Zeus-s <kristabdixx@gmail.com>
2022-10-16 17:06:29 +0700

    delete unused func

--- MinimaxBot.py
+++ MinimaxBot.py
@@ -21,70 +21,6 @@ class MinimaxBot(Bot):
         print(bestValue,bestAction)
         return bestAction
 
-    def get_num_chain_loop(self, state: GameState):
-        num_loop = 0
-        num_chain = 0
-        num_row, num_col = state.board_status.shape
-
-        temp = np.zeros(shape=(num_row, num_col))
-        # Mencari Kotak yang sudah Terbentuk
-        for locX, locY in np.argwhere(state.board_status == 4):
-            temp[locX, locY] = -1
-        for locX, locY in np.argwhere(state.board_status == -4):
-            temp[locX, locY] = -1
-
-        # Mencari Chain
-        still_search_chain = True
-        while (still_search_chain):
-            is_loop = False
-            loc_zero = np.argwhere(temp == 0)
-            if(len(loc_zero)==0):
-                break
-            else :
-                loc_zero = loc_zero[0]
-            first_el_chain = loc_zero
-            # print(loc_zero)
-            count_len_chain = 1
-            if (len(loc_zero) != 0):
-                x = loc_zero[0]
-                y = loc_zero[1]
-                temp[x, y] = count_len_chain
-                while (True):
-                    isMoving = True
-                    # Kanan Bawah Kiri Atas
-                    if (y+1 < num_col and isMoving):
-                        if (state.col_status[x][y+1] == 0 and temp[x, y+1] == 0):
-                            y += 1
-                            isMoving = False
-                    if (x+1 < num_row and isMoving):
-                        if (state.row_status[x+1][y] == 0 and temp[x+1, y] == 0):
-                            x += 1
-                            isMoving = False
-                    if (y-1 >= 0 and isMoving):
-                        if (state.col_status[x][y] == 0 and temp[x, y-1] == 0):
-                            y -= 1
-                            isMoving = False
-                    if (x-1 >= 0 and isMoving):
-                        if (state.row_status[x][y] == 0 and temp[x-1, y] == 0):
-                            x -= 1
-                            isMoving = False
-                    if isMoving:
-                        break
-
-                    if (temp[x, y] == 0):
-                        count_len_chain += 1
-                        temp[x, y] = count_len_chain
-                        is_loop = False
-                    elif (temp[x, y] == 1 and x == first_el_chain[0] and y == first_el_chain[1]):
-                        num_loop += 1
-                        is_loop = True
-                        break
-                if (not is_loop and count_len_chain >= max(num_row, num_col)):
-                    num_chain += 1
-            still_search_chain = np.any(temp == 0)
-
-        return num_chain, num_loop
-
     def utility_value(self, state : GameState) -> int:
         f = 0
         if (self.bot_code):
@@ -139,7 +75,7 @@ class MinimaxBot(Bot):
         if scored :
             return state_copy.player1_turn, state_copy
         else:
-            #state_copy.player1_turn = not state_copy.player1_turn
+            # state_copy.player1_turn = not state_copy.player1_turn
             return not state_copy.player1_turn, GameState(board_status=state_copy.board_status,row_status=state_copy.row_status,col_status=state_copy.col_status,player1_turn=not state_copy.player1_turn)#state_copy
 
     def get_best_step(self, alpha : int, beta : int, value : int, bestValue : int, isIteratingRow : bool, coord : (int, int), bestStep : GameAction, isMaximize : bool) -> (int, GameAction, int):
@@ -209,7 +145,6 @@ class MinimaxBot(Bot):
                 if beta <= alpha :
                     break
                 
-
         return bestValue, bestStep
 
 # if __name__=='__main__':
